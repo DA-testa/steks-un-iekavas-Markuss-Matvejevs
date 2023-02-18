ca6a05e198e7c0b02b92a8e02158215df5c501e4
Markuss-Matvejevs <markussm@inbox.lv>
2023-02-18 16:15:59 +0200

    completed task

--- main.py
+++ main.py
@@ -11,21 +11,28 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    position = 0
+    validPair = True
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(next)
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
-
+            validPair = are_matching(opening_brackets_stack[len(opening_brackets_stack)-1], next)
+            opening_brackets_stack.pop()
+        position += 1
+        if validPair == False: return position
+    return False
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    if mismatch == 0:
+        print("Success")
+    else:
+        print(mismatch)
 
 if __name__ == "__main__":
     main()
